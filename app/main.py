37b96940be6eaacbff72607d2252827dcbb26f87
Beka188 <esil.seitkalyk@gmail.com>
2024-03-04 06:34:25 +0500

    It returns error when tries to add the same user (same email) again

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, HTTPException
 from User import add_user, User
 
 app = FastAPI()
@@ -7,7 +7,11 @@ app = FastAPI()
 @app.post("/auth/users")
 def sign_up(email: str = Form(...), phone: str = Form(...), password: str = Form(...), name: str = Form(...),
             city: str = Form(...)):
-    add_user(email, phone, password, name, city)
+    status_code = add_user(email, phone, password, name, city)
+    if status_code == 200:
+        return {"message": "User created successfully"}
+    else:
+        raise HTTPException(status_code=409, detail="User already exists")
 
 
 if __name__ == '__main__':
