733abeb7ee2f429b3c9fa5fad38d2ebe09b615fb
Beka188 <esil.seitkalyk@gmail.com>
2024-03-04 12:52:22 +0500

    It now works for request, but I was not able to perform request via docs, works fine with curl

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI, Form, HTTPException
-from User import add_user, User, login, print_all
-from pydantic import BaseModel
-import hashlib
+from fastapi import FastAPI, Form, HTTPException, Request
+from User import add_user, login, print_all
+
 
 app = FastAPI()
 
@@ -21,11 +20,8 @@ def sign_up(email: str = Form(...), phone: str = Form(...), password: str = Form
         raise HTTPException(status_code=409, detail="User already exists")
 
 
-@app.get("/auth/users/login", response_model=None)
-def user_login(username: str, password: str):
-    # username = request.username
-    # password = request.username
-    # return 200
+@app.post("/auth/users/login")
+def user_login(username: str = Form(...), password: str = Form(...)):
     if login(username, password):
         return 200  # also jwt token
     else:
@@ -33,7 +29,7 @@ def user_login(username: str, password: str):
 
 
 if __name__ == '__main__':
-    add_user("email", "ph", "pas", "name", "city")
     print_all()
+
 
 
